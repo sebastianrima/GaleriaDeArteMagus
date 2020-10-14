7c323db1735291d2d9d27b56bcfaa2c9cdb7c99b
isaacHeYe704 <isaacheye@unisabana.edu.co>
2020-10-14 11:51:11 -0500

    a

--- src/main/webapp/mainLogic.js
+++ src/main/webapp/mainLogic.js
@@ -11,10 +11,27 @@ function requestTemporada()
     xhttp.send();
     
 }
+    
+function mostrarDatos(respuesta)
+{
+    var datosSeparadosTemporada = respuesta.split("ñ");
+    for (var i=0; i < datosSeparadosTemporada.length; i++) {
+      alert(datosSeparadosTemporada[i]);
+   }
+   var titulo= datosSeparadosTemporada[0];
+   var img1 = datosSeparadosTemporada[1];
+   var img2 = datosSeparadosTemporada[2];
+   var textoTemporada = datosSeparadosTemporada[3];
+   
+   document.getElementById("NombreTemporadaActual").innerHTML = titulo;    
+   document.getElementById("contenedorCaracteristicasTemporada").innerHTML = textoTemporada;    
+   document.getElementById("imagen1").src = img1;
+   document.getElementById("imagen2").src = img2;
+}
 xhttp.onreadystatechange = function ()
 {
     if (this.readyState === 4 && this.status === 200)
     {
-        alert(this.responseText);
+        mostrarDatos(this.responseText); 
     }
 }
