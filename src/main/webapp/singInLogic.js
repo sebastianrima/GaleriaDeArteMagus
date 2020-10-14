3b34319e8cdfd8269fe013f52ec296ac46508426
isaacHeYe704 <isaacheye@unisabana.edu.co>
2020-10-14 09:29:57 -0500

    añado lo que ya tniamos del login

---
+++ src/main/webapp/singInLogic.js
@@ -0,0 +1,23 @@
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function ()
+{
+    if (this.readyState === 4 && this.status === 200)
+    {
+        alert(this.responseText);
+    }
+}
+function guardarCliente()
+{
+    //recolectar info
+    var Name = document.getElementById("nombre").value;
+    var userName = document.getElementById("userName").value;
+    var password = document.getElementById("pass").value;
+    var correo = document.getElementById("correo").value;
+    var telefono = document.getElementById("telefono").value;
+    var myInfo =Name+","+userName+","+password+","+correo+","+telefono;
+ 
+    xhttp.open("GET", "guardarCliente?data="+myInfo, true);
+    xhttp.send();
+
+}
