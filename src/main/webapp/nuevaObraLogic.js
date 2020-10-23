14841fb59c0b9989ab9f585b3e8ff844e1d6e3bb
Nickonix1 <62396717+Nickonix1@users.noreply.github.com>
2020-10-23 00:11:48 -0500

    Validaciones new obra

--- src/main/webapp/nuevaObraLogic.js
+++ src/main/webapp/nuevaObraLogic.js
@@ -7,44 +7,45 @@ xhttp.onreadystatechange = function ()
 {
     if (this.readyState === 4 && this.status === 200)
     {
-           alert(this.responseText);
-           mostrarArtistas(this.responseText);
-           fillAutorSelect(this.responseText);
+        alert(this.responseText);
+        mostrarArtistas(this.responseText);
+        fillAutorSelect(this.responseText);
     }
 }
 
-function mostrarArtistas( respuesta){
+function mostrarArtistas(respuesta) {
     var respuestaSplited = respuesta.split(",");
-    var opciones="";
-    
-    
-    for(var i=0; i<respuestaSplited.length;i+=2)
+    var opciones = "";
+
+
+    for (var i = 0; i < respuestaSplited.length; i += 2)
     {
-        opciones+="<option value=\""+respuestaSplited[i]+"\">"+respuestaSplited[i+1]+"</option>";
+        opciones += "<option value=\"" + respuestaSplited[i] + "\">" + respuestaSplited[i + 1] + "</option>";
     }
-    
-    document.getElementById("autorSelect").innerHTML=opciones;
-    
+
+    document.getElementById("autorSelect").innerHTML = opciones;
+
 }
 
-window.onload = function() {
+window.onload = function () {
     requestAutorInfo();
 };
 
-function requestAutorInfo(){
+function requestAutorInfo() {
     xhttp.open("GET", "nuevaObraServlet", true);
     xhttp.send();
 }
 
-function fillAutorSelect(respuesta){
+function fillAutorSelect(respuesta) {
     var datosSeparados = respuesta.split(",");
-    
+
     //For hasta largo arreglo
-    
+
     document.getElementById("autorSelect");
 }
-function guardarObra()
-{
+
+
+function validarDatos() {
     //recolectar info
     var nombre = document.getElementById("nombreText1").value;
     var descripcion = document.getElementById("descripcionText1").value;
@@ -53,20 +54,34 @@ function guardarObra()
     var tipoTexto = document.getElementById("TipoSelector");
     var tipo = tipoTexto.options[tipoTexto.selectedIndex].value;
     var colorTexto = document.getElementById("colorSelect");
-    var color= colorTexto.options[colorTexto.selectedIndex].text;
-    alert(color);
+    var color = colorTexto.options[colorTexto.selectedIndex].text;
     var emocion = document.getElementById("emocionText1").value;
     var tematica = document.getElementById("tematicaText1").value;
     var movimiento = document.getElementById("movimientoText1").value;
     var imagen = document.getElementById("urlText1").value;
     var autorTexto = document.getElementById("autorSelect");
-    var autor= autorTexto.options[autorTexto.selectedIndex].value;
-    
-    var myInfo = nombre+",,"+precioBase+",,"+imagen+",,"+fecha+",,"+
-    tipo+",,"+color+",,"+emocion+",,"+tematica+",,"+movimiento+",,"+descripcion+",,"+autor;
-    
+    var autor = autorTexto.options[autorTexto.selectedIndex].value;
+
+    if (nombre == "" || descripcion == "" || precioBase == "" || fecha == "" || tipo == "" || color == "Escoger" || emocion == "" || tematica == "" || movimiento == "" || imagen == "" || autor == "") {
+        alert("Llene los campos...");
+    } else {
+        if (isNaN(precioBase)) {
+            alert("El precio debe ser un número...");
+        } else {
+            alert("todo melo");
+            //guardarObra(nombre, precioBase, imagen, fecha, tipo, color, emocion, tematica, movimiento, descripcion, autor);
+        }
+
+    }
+
+}
+function guardarObra(nombre, precioBase, imagen, fecha, tipo, color, emocion, tematica, movimiento, descripcion, autor)
+{
+
+    var myInfo = nombre + ",," + precioBase + ",," + imagen + ",," + fecha + ",," +
+            tipo + ",," + color + ",," + emocion + ",," + tematica + ",," + movimiento + ",," + descripcion + ",," + autor;
     alert(myInfo);
-    xhttp1.open("GET", "guardarObra?data="+myInfo, true);
+    xhttp1.open("GET", "guardarObra?data=" + myInfo, true);
     xhttp1.send();
 }
 
@@ -75,11 +90,10 @@ xhttp1.onreadystatechange = function ()
 {
     if (this.readyState === 4 && this.status === 200 && this.responseText !== "")
     {
-           alert(this.responseText);
-           alert("La obra se guardo correctamenta");
-           goToMain();
-    }
-    else if(this.readyState === 4 && this.status === 200)
+        alert(this.responseText);
+        alert("La obra se guardo correctamenta");
+        goToMain();
+    } else if (this.readyState === 4 && this.status === 200)
     {
         alert("¡ha ocurrido un problema con el servidor por favor intente más tarde!");
     }
@@ -87,7 +101,7 @@ xhttp1.onreadystatechange = function ()
 
 function goToMain()
 {
-    top.window.location="/main.html";
+    top.window.location = "/main.html";
 }
 
 
